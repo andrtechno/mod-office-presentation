3ab5db3a0928e2d7c2e258f77cbf29c2218be334
Andrew <andrew.panix@gmail.com>
2019-04-26 06:04:34 +0300

    init

---
+++ models/query/PresentationQuery.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace panix\mod\presentation\models\query;
+
+use panix\engine\traits\query\DefaultQueryTrait;
+use yii\db\ActiveQuery;
+
+class PresentationQuery extends ActiveQuery {
+
+    use DefaultQueryTrait;
+}
---
+++ views/admin/settings/index.php
@@ -0,0 +1,26 @@
+<?php
+
+use panix\engine\Html;
+use panix\engine\bootstrap\ActiveForm;
+?>
+
+
+<div class="card">
+    <div class="card-header">
+        <h5><?= $this->context->pageName ?></h5>
+    </div>
+    <div class="card-body">
+        <?php $form = ActiveForm::begin(); ?>
+        <?= $form->field($model, 'auth_login') ?>
+        <?= $form->field($model, 'auth_token') ?>
+        <?= $form->field($model, 'account') ?>
+
+
+        <div class="card-footer text-center">
+            <?= Html::submitButton(Yii::t('app', 'SAVE'), ['class' => 'btn btn-success']) ?>
+        </div>
+        <?php ActiveForm::end(); ?>
+    </div>
+
+</div>
+
